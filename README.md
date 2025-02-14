4d8ea61a4287f87f399d0034ba00a93887b8c320
Petros Andreou <pandreou2010@gmail.com>
2025-02-14 02:12:32 +0000

    Run the SD-3 medium after tweaking the parameters

--- README.md
+++ README.md
@@ -7,19 +7,19 @@ Ensure you have python 3.11.9 installed.
 * Virtual env setup:
 To set up a virtual environment run the following:
 ```commandline
-py -m venv env
-env\Scripts\activate
+python -m venv .env
+.env\Scripts\activate
 ```
 That should enable a virtual environment for you to start working with
 
 * Install all necessary requirements by running:
-`py -m pip install requirements.txt`
+`python -m pip install requirements.txt`
 
 * You will need Nvidia CUDA drivers to run pytorch, you can find
 the installation [here](https://pytorch.org/get-started/locally/). Just make sure to choose Pip
 and the CUDA version you have, which you can find by running `nvcc --version`.
 If everything is installed then run:
-`py -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126`
+`python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126`
 
 WARNING: If you don't install this way then you will see the following error: `AssertionError: Torch not compiled with CUDA enabled`
 
--- main.py
+++ main.py
@@ -2,24 +2,32 @@ import torch
 from diffusers import StableDiffusion3Pipeline
 from PIL import Image
 
-# Replace the model version with your required version if needed
+torch.set_float32_matmul_precision("high")
+
+model_id = "stabilityai/stable-diffusion-3-medium-diffusers"
+
+
 pipeline = StableDiffusion3Pipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers",
-    torch_dtype=torch.float16
+    model_id,
+    torch_dtype=torch.float16,
+    text_encoder_3=None,
+    tokenizer_3=None
 )
+pipeline.set_progress_bar_config(disable=True)
 
 # Running the inference on GPU with cuda enabled
-pipeline = pipeline.to('cuda')
-
-prompt = ("a slender punk female sitting reclined against the wall of a dark street savoring a short cigarette, on the wall spray painted in red "
-          "\"Punk\'s Not Dead\", hyper realistic, detailed dreads pink and black hair, side mohawk haircut, large necklaces, tattoos on all body, nose lip eyebrow piercing, worn crop tank top, dirty and torn apart cloths, wide dark background, luis royo illustration, rembrandt lighting, concept art, fantasy art, hyper detailed, intricate, sharp focus, best quality, masterpiece")
+prompt = ("slender punk female sitting reclined against the wall of a dark street savoring a short cigarette,"
+          "hyper realistic, detailed dreads pink and black hair, side mohawk haircut, large necklaces, tattoos on all body, nose lip eyebrow piercing, worn crop tank top, dirty and torn apart cloths, wide dark background, luis royo illustration, rembrandt lighting, concept art, fantasy art, hyper detailed, intricate, sharp focus, best quality, masterpiece")
 
 image = pipeline(
     prompt=prompt,
     negative_prompt="",
     num_inference_steps=28,
+    max_sequence_length=128,
     guidance_scale=7.0,
 ).images[0]
+pipeline = pipeline.to('cuda')
+
 
 if __name__ == "__main__":
     # torch.cuda.is_available()
